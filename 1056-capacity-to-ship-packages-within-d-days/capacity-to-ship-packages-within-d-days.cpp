9d9d51fdff5bb04bbeed042229d09fa921b5dcd1
SrishtiAnand <145741807+SrishtiAnand@users.noreply.github.com>
2026-07-10 10:38:25 +0530

    Time: 14 ms (36.18%) | Memory: 35.1 MB (53.93%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& w, int days) {
+
+         int n = w.size();
+        int low =0, high=0;
+        int mid =0;
+        for(int i=0; i<n; i++){
+            low = max(low,w[i]);
+            high+=w[i];
+        }
+        long ans =high;
+            while(low<=high){
+             mid = low+(high-low)/2;
+             
+        int requiredDays = 1;
+        int currentWeight = 0;
+        for (int weight : w) {
+         if (currentWeight + weight <= mid) {
+          currentWeight += weight;
+    }     else {
+         requiredDays++;
+         currentWeight = weight;
+    }
+}
+           if(requiredDays<=days){
+            ans =requiredDays;
+           high =mid-1;
+            }else low =mid+1;
+
+            }
+            return low;
+    }
+};
