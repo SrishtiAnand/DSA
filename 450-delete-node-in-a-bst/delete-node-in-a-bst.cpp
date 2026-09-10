5de103169b53192b335656dea7039ee480a0a370
SrishtiAnand <145741807+SrishtiAnand@users.noreply.github.com>
2026-09-10 12:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 34.5 MB (15.62%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,32 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==NULL) return NULL;
-
-        if(root->val==key && root->left == NULL) return root->right;
-        if(root->val==key && root->right == NULL) return root->left;
-
-        if(key<root->val){
+        if (root == NULL)
+            return NULL;
+        if (root->val > key) {
             root->left = deleteNode(root->left, key);
-        }
-        else if(key> root->val){
+        } else if (root->val < key) {
             root->right = deleteNode(root->right, key);
-        }
-        else {
-            TreeNode* temp = root->right;
-            while(temp->left != NULL)
-                temp = temp->left;
+        } else {
+            if(root->left ==NULL)return root->right ;
+            if(root->right ==NULL)return root->left;
+            TreeNode* t = root->right;
+            TreeNode* lastleft = root->right;
+           
+            while (lastleft->left != NULL) {
+                lastleft= lastleft->left;
+            }
+            
 
-            root->val = temp->val;
-            root->right = deleteNode(root->right, temp->val);
+            lastleft->left = root->left;
+            
+            
+            return t;
+           
         }
-
         return root;
     }
 };
